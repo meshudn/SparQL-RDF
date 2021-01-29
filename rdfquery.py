7fc71ed0b9af432f1ab0ca85b96416b05d72dc6b
Meshu Deb Nath <meshu.uiu@gmail.com>
2021-01-29 01:51:38 +0100

    Update rdfquery.py

--- rdfquery.py
+++ rdfquery.py
@@ -1,10 +1,8 @@
 import rdflib
 import requests
 g = rdflib.Graph()
-g2 = rdflib.Graph()
 
 g.parse("dummy.rdf")
-g2.parse("expertise.rdf")
 
 qres = g.query(
     """SELECT DISTINCT ?name ?expertise
@@ -16,13 +14,3 @@ qres = g.query(
 for row in qres:
     #print("%s %s" % row)
     value = row
-
-qres2 = g2.query(
-        """SELECT DISTINCT ?name
-           WHERE {
-              ?name ?predicate ?object .
-              FILTER (?name = "http://wirtrials.app.web/researchee#expertise1")
-           }
-           """)
-for row2 in qres2:
-     print("%s " % row2)
