dab02ff074d213a980e10baaa1089d653ae6bb06
Nafisa Islam <islamnafisa94@gmail.com>
2021-01-30 23:42:31 +0100

    added change for rdf object

--- rdfquery.py
+++ rdfquery.py
@@ -1,28 +1,16 @@
 import rdflib
 import requests
 g = rdflib.Graph()
-g2 = rdflib.Graph()
-
 g.parse("dummy.rdf")
-g2.parse("expertise.rdf")
 
 qres = g.query(
-    """SELECT DISTINCT ?name ?expertise
+    """SELECT DISTINCT ?a
        WHERE {
-          ?a researchee:name ?name .
-          ?a researchee:hasExpertise ?expertise .
+          ?a researchee:hasExpertise ?name .
        }""")
 
 for row in qres:
-    #print("%s %s" % row)
+    print("%s " % row)
     value = row
 
-qres2 = g2.query(
-        """SELECT DISTINCT ?name
-           WHERE {
-              ?name ?predicate ?object .
-              FILTER (?name = "http://wirtrials.app.web/researchee#expertise1")
-           }
-           """)
-for row2 in qres2:
-     print("%s " % row2)
+
